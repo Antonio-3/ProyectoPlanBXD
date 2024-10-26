6f3cc4b229f9f1ccb51e53e0f46ad8b809f7212c
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-25 19:04:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,11 +75,9 @@ if seleccion_menu == "Jefe de grupo":
                                 st.write("Si asistio")
                         if st.button("No asistio"):
                                 st.write("No asistio")
-                        left, middle, right = st.columns(3)
+                        left, right = st.columns(2)
                         if left.button("Plain button", use_container_width=True):
                             left.markdown("You clicked the plain button.")
-                        if middle.button("Emoji button", icon="😃", use_container_width=True):
-                            middle.markdown("You clicked the emoji button.")
                         if right.button("Material button", icon=":material/mood:", use_container_width=True):
                             right.markdown("You clicked the Material button.")
 
