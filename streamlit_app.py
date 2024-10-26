1daf4ef6dfaf4519964cec1f0fdddb82be1ac283
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-25 19:47:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,9 +152,6 @@ if seleccion_menu == "Jefe de grupo":
         
         if seleccion_reporte == "Reporte por profesor":
                 st.write("  \n")
-                st.write("  \n")
-                st.write("  \n")
-                st.write("  \n")
                 st.title("Reporte por profesor")
                 # Conectar a la base de datos
                 conexion = sqlite3.connect('BasePrueba/ProfesoresPrueba.db')
