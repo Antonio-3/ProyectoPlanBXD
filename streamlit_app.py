f28392a83af7abdbd78d188742f782c42b8bb05c
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-27 10:13:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,16 +194,14 @@ if seleccion_menu == "Jefe de grupo":
                         pdf.set_auto_page_break(auto=True, margin=15)
                         # Agregar una página
                         pdf.add_page()
-                        # Establecer el tipo de fuente (Arial, negrita, tamaño 16)
                         pdf.set_font('Arial', 'B', 16)
                         # Título del reporte
                         pdf.cell(200, 10, 'Reporte de Profesor', ln=True, align='C')
-                        # Espacio adicional
                         pdf.ln(10)
-                        # Establecer el tipo de fuente para el contenido 
+                        #tipo de fuente para el contenido 
                         pdf.set_font('Arial', '', 12)
 
-                        # Encabezados de la tabla ajustados
+                        # Encabezados de la tabla
                         pdf.cell(10, 10, 'ID', 1)     
                         pdf.cell(45, 10, 'Profesor', 1)  
                         pdf.cell(40, 10, 'Materia', 1)    
@@ -213,7 +211,7 @@ if seleccion_menu == "Jefe de grupo":
                         pdf.cell(20, 10, 'Asistencia', 1) 
                         pdf.ln()
                         
-                        # Agregar los registros de materias al PDF con ajustes
+                        # Agregar los registros de materias al PDF
                         for materia in materiaprofe:
                             pdf.cell(10, 10, str(materia[0]), 1)   
                             pdf.cell(45, 10, materia[1], 1)        
@@ -239,8 +237,6 @@ if seleccion_menu == "Jefe de grupo":
                         
                         # Cerrar la conexión
                         conexion.close()
-                        # Retornar el contenido del PDF en bytes
-                        return pdf.output(dest='S').encode('latin1')
                 # Botón para generar el PDF
                 if st.button("Generar Reporte"):
                         # Generar el PDF
